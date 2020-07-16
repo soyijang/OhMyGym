b3642795ef95dcb8e7c3e2acdb8ddb43aa25787d
USER <USER@DESKTOP-PTGTKCO>
2020-07-16 21:33:20 +0900

    팔로워 건강정보 입력 기능 구현(미완성)

--- src/com/omg/jsp/followerHealth/controller/InsertHealthHistoryServlet.java
+++ src/com/omg/jsp/followerHealth/controller/InsertHealthHistoryServlet.java
@@ -11,19 +11,18 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.omg.jsp.followerHealth.model.service.HealthInfoService;
 import com.omg.jsp.followerHealth.model.vo.HealthInfo;
-import com.omg.jsp.member.model.vo.Member;
 
 /**
- * Servlet implementation class HealthinfoServlet
+ * Servlet implementation class UpdateFollowerHealthInfo
  */
-@WebServlet("/healthInfo.hi")
-public class HealthinfoServlet extends HttpServlet {
+@WebServlet("/insertHistory.hi")
+public class InsertHealthHistoryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HealthinfoServlet() {
+    public InsertHealthHistoryServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -34,28 +33,41 @@ public class HealthinfoServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		
-		System.out.println("HealthinfoServlet");
+		String[] sarr = new String[10];
 		
-		Member loginUser = (Member) request.getSession().getAttribute("loginUser");
+		sarr[0] = request.getParameter("userId");
+		sarr[1] = request.getParameter("height");
+		sarr[2] = request.getParameter("weight");
+		sarr[3] = request.getParameter("sleep");
+		sarr[4] = request.getParameter("exercise");
+		sarr[5] = request.getParameter("focus");
+		sarr[6] = request.getParameter("active");
+		sarr[7] = request.getParameter("motive");
+		sarr[8] = request.getParameter("bodyUneasy");
+		sarr[9] = request.getParameter("goal");
 		
-		ArrayList<HealthInfo> list = new HealthInfoService().selectHealthInfo(loginUser);
-
-		String page = "";
-		if(list != null) {
-			page = "views/follower/followerHealth/followerInsertHealthInfo.jsp";
-			request.setAttribute("list", list);
-		} else {
-			page = "views/common/errorPage.jsp";
-			request.setAttribute("msg", "HealthInfo 조회 실패");
+		System.out.println("UpdateFollowerHealthInfoServlet");
+		
+		ArrayList<HealthInfo> list = new ArrayList<HealthInfo>();
+		
+		for(int i = 0; i < sarr.length - 1; i++) {
+			HealthInfo hi = new HealthInfo(sarr[0], sarr[i + 1], i + 1 + "");
+			list.add(hi);
 		}
 		
-		request.getRequestDispatcher(page).forward(request, response);
+		System.out.println("InsertHealthHistoryServlet");
+		System.out.println("servlet list : " + list);
+		
+		int result = new HealthInfoService().insertHealthHistory(list);
+		
+		
+		
+		
+		
 		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
